aa26fed72588431cd4022684d8cc888ddacd94cb
Badreddine Gharbi <blackbird@Badreddines-Air.home>
2019-09-15 16:01:07 -0700

    Updates

---
+++ precache-manifest.c7940896ece570a4e853b2eb9f2d4cb9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9881e67fb72b4b419ec0",
+    "url": "/todo_app/static/js/runtime~main.9881e67f.js"
+  },
+  {
+    "revision": "122cfd2f95f0c1c260c8",
+    "url": "/todo_app/static/js/main.122cfd2f.chunk.js"
+  },
+  {
+    "revision": "a46c3e295f5ff88ce5c1",
+    "url": "/todo_app/static/js/1.a46c3e29.chunk.js"
+  },
+  {
+    "revision": "122cfd2f95f0c1c260c8",
+    "url": "/todo_app/static/css/main.c09d847d.chunk.css"
+  },
+  {
+    "revision": "a7ec2c93afbffadaefa56e484d1bf230",
+    "url": "/todo_app/index.html"
+  }
+];
